899a163cf61e2cab4d73a81b7059d1bc2fd8a80c
WMascot <danila33366@gmail.com>
2022-12-20 21:03:17 +0300

    Created service extension to add repositories

---
+++ CashViewer-backend.BAL/Extensions/RepositoriesRegister.cs
@@ -0,0 +1,17 @@
+﻿using CashViewer_backend.DAL.Entities;
+using CashViewer_backend.DAL.Interfaces;
+using CashViewer_backend.DAL.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashViewer_backend.BAL.Extensions
+{
+    public static class RepositoriesRegister
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services) =>
+            services.AddScoped<IRepository<Bill>, BillsRepository>()
+                    .AddScoped<IRepository<BillProduct>, BillProductsRepository>()
+                    .AddScoped<IRepository<Product>, ProductsRepository>()
+                    .AddScoped<IRepository<Shop>, ShopsRepository>()
+            ;
+    }
+}
