f4d9867096c60ec1a72dc39dc9c9d0efe9101fc9
WMascot <danila33366@gmail.com>
2022-12-20 20:12:02 +0300

    Data access layer setup

---
+++ CashViewer-backend.DAL/Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CashViewer_backend.DAL.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ CashViewer-backend.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace CashViewer_backend.DAL.Interfaces
+{
+    public interface IRepository<T> where T: class, IEntity, new()
+    {
+        IQueryable<T> Items { get; }
+        T Get(int id);
+        Task<T> GetAsync(int id, CancellationToken cancellationToken = default);
+        T Add(T entity);
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+        void Update(T entity);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        void Remove(int id);
+        Task RemoveAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
