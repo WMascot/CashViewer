c5f97461e59b4e9ed0e820430012086af51051e1
WMascot <danila33366@gmail.com>
2022-12-20 21:49:54 +0300

    Configured db connection

--- CashViewer-backend.API/Program.cs
+++ CashViewer-backend.API/Program.cs
@@ -1,7 +1,18 @@
+using CashViewer_backend.BAL.Extensions;
+using CashViewer_backend.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("MySQL");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    })
+    .AddRepositories();
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
