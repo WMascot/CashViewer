21eaaf714401d573c46c5b1a9746a15e6aa5ad6b
WMascot <danila33366@gmail.com>
2022-12-20 20:58:58 +0300

    Added db sets

--- CashViewer-backend.DAL/Context/ApplicationDbContext.cs
+++ CashViewer-backend.DAL/Context/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CashViewer_backend.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CashViewer_backend.DAL.Context
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillProduct> BillProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shop> Shops { get; set; }
     }
 }
