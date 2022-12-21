58a3f372daba68242ad34418b6547d680aa10b36
WMascot <danila33366@gmail.com>
2022-12-21 14:24:35 +0300

    Added extension for services to add BAL services

---
+++ CashViewer-backend.BAL/Extensions/ServicesRegister.cs
@@ -0,0 +1,12 @@
+﻿using CashViewer_backend.BAL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashViewer_backend.BAL.Extensions
+{
+    public static class ServicesRegister
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) =>
+            services.AddScoped<ServiceShop>()
+            ;
+    }
+}
