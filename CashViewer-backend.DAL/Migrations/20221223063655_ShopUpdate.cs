4da50c15b3649d73b07f4a9807aa4da5f0a666a3
WMascot <danila33366@gmail.com>
2022-12-23 09:39:36 +0300

    Updated database

---
+++ CashViewer-backend.DAL/Migrations/20221223063655_ShopUpdate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashViewer_backend.DAL.Migrations
+{
+    public partial class ShopUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Shops",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Shops");
+        }
+    }
+}
